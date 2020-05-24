bc9efed7a90089951406773453a22aabf33e2e64
Julio Cesar Urbano Vasconcelos Filho <julio.vasconcelos@academico.ifpb.edu.br>
2020-05-24 15:03:16 -0300

    fundoPreto

--- src/component/CameraOverlay.js
+++ src/component/CameraOverlay.js
@@ -1,30 +1,26 @@
 import React from 'react';
-import { Dimensions, View, Text } from 'react-native';
-import Svg, { Circle, Defs, Rect, Mask } from 'react-native-svg';
+import {Dimensions, View} from 'react-native';
+import Svg, {Circle, Defs, Rect, Mask} from 'react-native-svg';
 
 const CameraOverlay = () => {
-  const { height, width } = Dimensions.get('window');
+  const {height, width} = Dimensions.get('window');
   const circleRadius = width / 2.5;
-  const viewBox = `0 0 ${width} ${height}`
+  const viewBox = `0 0 ${width} ${height}`;
   return (
     <View aspectRatio={1}>
-      <Svg
-        height={height}
-        viewBox={viewBox}
-      >
+      <Svg height={height} viewBox={viewBox}>
         <Defs>
           <Mask id="mask">
-            <Rect height={height} width={width} fill="#fff" />
-            <Circle r={circleRadius} cx={width / 2} cy={height / 3} fill="#000" />
+            <Rect height={height} width={width} fill="#ffffff" />
+            <Circle
+              r={circleRadius}
+              cx={width / 2}
+              cy={height / 3}
+              fill="#000"
+            />
           </Mask>
         </Defs>
-
-        <Rect
-          height={height}
-          width={width}
-          fill="#ffffff"
-          mask="url(#mask)"
-        />
+        <Rect height={height} width={width} fill="#000" mask="url(#mask)" />
       </Svg>
     </View>
   );
