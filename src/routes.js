7099c68d08b34bfe53c06f8053edffeaf1edb133
Julio Cesar Urbano Vasconcelos Filho <julio.vasconcelos@academico.ifpb.edu.br>
2020-05-24 14:43:00 -0300

    estruturacao Pages e navegacao

---
+++ src/routes.js
@@ -0,0 +1,17 @@
+import 'react-native-gesture-handler';
+import React from 'react';
+import {createStackNavigator} from '@react-navigation/stack';
+import {NavigationContainer} from '@react-navigation/native';
+
+import Selfie from './pages/SelfiePage';
+const AppStack = createStackNavigator();
+
+export default function Routes() {
+  return (
+    <NavigationContainer>
+      <AppStack.Navigator screenOptions={{headerShown: false}}>
+        <AppStack.Screen name="Selfie" component={Selfie} />
+      </AppStack.Navigator>
+    </NavigationContainer>
+  );
+}
